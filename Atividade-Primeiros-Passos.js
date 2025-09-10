f334f99291b827696e4c1b696d89a4af1e0287b5
M3LLOUU <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-10 19:30:52 -0300

    Diferença de variaveis, de Portugol para JavaScript

---
+++ Atividade-Primeiros-Passos.js
@@ -0,0 +1,25 @@
+// Atividade: Portugol para JavaScript
+
+// Declaração de variáveis
+
+/* em Portugol:
+
+    cadeia nome = "Jaime";
+    inteiro idade = 20;
+    real altura = 1.77;
+    logico estudante = verdadeiro;
+
+*/
+
+let nome   = "Jaime";
+let idade  = 20;
+let altura = 1.77;
+let estudante = true;
+
+
+console.log("Hello, world!");
+
+console.log("Nome: " + nome); // escreva(""Nome: ", nome);
+console.log("Idade: " + idade); // escreva("Idade: ", idade);
+console.log("Altura: " + altura); // escreva("Altura: ", altura);
+console.log("Estudante: " + estudante); // escreva("Estudante: ", estudante);
