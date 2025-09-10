2eaef6c78877b54f6cd2a70c22f31419d18328b3
Jaime <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-10 23:52:37 +0000

    Adiciona estrutura condicional para aprovação e reprovação com IF e ELSE (se / senao)

---
+++ condiconais.js
@@ -0,0 +1,14 @@
+/*
+    se (nota >= 7.0){
+        escreva("Aprovado")
+    } senao {
+        escreva("Reprovado") 
+    }
+*/
+let nota = 8;
+
+if (nota >= 7.0) {
+    console.log("Aprovado!");
+} else {
+    console.log("Reprovado!");
+}
