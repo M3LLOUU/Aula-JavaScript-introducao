56947630fc73b036ea6dc19d713e0e96a8bd6bb9
Jaime <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-10 23:43:15 +0000

    Adiciona exercício prático com informações pessoais e console.log de perfil.

---
+++ exercicio-pratico-um.js
@@ -0,0 +1,8 @@
+let meu_nome = "Jaime";
+let minha_idade = 25;
+let minha_cidade = "Pomerode - SC";
+let gosta_de_programar = true
+
+console.log(`Meu nome é ${meu_nome}, tenho ${minha_idade} anos`);
+console.log(`Moro em ${minha_cidade}`);
+console.log("Gosta de programar: ", gosta_de_programar);
