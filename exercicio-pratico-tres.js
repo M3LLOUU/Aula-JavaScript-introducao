5723ab110e08f1ca7bbb3276624d187af9242a87
Jaime <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-11 21:29:01 +0000

    Adiciona exercício prático de soma de números em Portugol e JavaScript

---
+++ exercicio-pratico-tres.js
@@ -0,0 +1,17 @@
+/* Portugol
+   inteiro soma = 0
+   para (inteiro i = 1; i <= 100; i++) {
+       soma = soma + i
+   }
+   escreva("A soma dos números de 1 a 100 é: ", soma)
+*/
+
+//JavaScript
+let soma = 0
+
+for (let i = 1; i <= 100; i++) {
+    soma += i
+}
+
+console.log("A soma dos números de 1 a 100 é: ", soma)
+
