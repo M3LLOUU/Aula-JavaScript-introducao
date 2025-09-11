f9e552fea8fbe3f67e6b27a47e1562211fea0227
Jaime <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-11 21:51:19 +0000

    Adiciona exemplos de métodos modernos de arrays: filter, map e reduce

---
+++ metodos-modernos-de-arrays.js
@@ -0,0 +1,14 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Filtrar elementos
+let pares = numeros.filter(n => n % 2 === 0);
+
+// Map - transformar elementos
+let quadrados = numeros.map(n => n * n);
+
+// Reduce - reduzir a um valor 
+let soma = numeros.reduce((acc, n) => acc + n, 0);
+
+console.log("Pares:", pares);
+console.log("Quadrados:", quadrados);
+console.log("Soma:", soma);
