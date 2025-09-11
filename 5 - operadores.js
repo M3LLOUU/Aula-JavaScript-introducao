c45f934cb86651b31b2329ad1b712e71c90819f8
Jaime <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-11 00:44:56 +0000

    Trocar nomes dos arquivos e alterar fontes

---
+++ 5 - operadores.js
@@ -0,0 +1,13 @@
+// igualdade
+
+console.log(5 = "5"); // true
+
+console.log(5 === "5"); // false
+
+console.log(5 != "5"); // false
+
+console.log(5 !== "5"); // true
+
+console.log(5 > 3); // true
+
+console.log(10 <= 10); // false
