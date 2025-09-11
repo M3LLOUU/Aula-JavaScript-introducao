c52044424bfac16675e607447e8a50cc6f859551
Jaime <jaime_neckel-junior@estudante.sesisenai.org.br>
2025-09-11 21:30:43 +0000

    Adiciona função para calcular média em JavaScript

---
+++ funcoes-comparacao.js
@@ -0,0 +1,14 @@
+/* Portugol
+
+funcao real calcularMedia(real n1, real n2) {
+    retorne (n1 + n2) / 2
+}
+*/
+
+//JavaScript
+function calcularMedia(n1, n2) {
+    return (n1 + n2) / 2;
+}
+
+let media = calcularMedia(7, 8);
+console.log("Média: ", media);
